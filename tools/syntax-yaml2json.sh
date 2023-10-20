1ae50fb5ddfa3c3f56e1096b5de1a04ddead844b
Dale Emmons <dpemmons@gmail.com>
2023-10-20 10:58:19 -0700

    yaml representations of syntax files and a little tool to convert them.

---
+++ tools/syntax-yaml2json.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/bash
+
+if [ ! -f ".typedef-vscode-root" ]; then
+    echo ".typedef-vscode-root does not exist. Are you running this script from the project root?"
+    exit 1
+fi
+
+set -xe
+
+npx js-yaml \
+  syntaxes/typedef-template.tmLanguage.yaml \
+  > syntaxes/typedef-template.tmLanguage.json
+
+npx js-yaml \
+  syntaxes/typedef-template.tmLanguage.yaml \
+  > syntaxes/typedef-template.tmLanguage.json
